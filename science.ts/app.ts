ea88a1b94d552ca10654ca68959d998e17a31eb7
xieguigang <xie.guigang@gcmodeller.org>
2019-10-11 16:12:56 +0800

    split module files

---
+++ science.ts/app.ts
@@ -0,0 +1,12 @@
+﻿namespace science {
+
+    /**
+     * semver
+    */
+    export const version: string = "1.9.3";
+
+    /**
+     * Euler's constant.
+    */ 
+    export const EULER = .5772156649015329;
+}
