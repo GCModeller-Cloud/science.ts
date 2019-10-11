6a3b984db7db4a2e1207a149dde4902a7241fa19
xieguigang <xie.guigang@gcmodeller.org>
2019-10-11 16:29:05 +0800

    Quadratic

--- science.ts/Math.ts
+++ science.ts/Math.ts
@@ -3,16 +3,17 @@
     export function ascending(a: number, b: number): number {
         return a - b;
     };
+
     // Compute exp(x) - 1 accurately for small x.
-    science.expm1 = function (x) {
+    export function expm1(x) {
         return (x < 1e-5 && x > -1e-5) ? x + .5 * x * x : Math.exp(x) - 1;
     };
-    science.functor = function (v) {
+    export function functor(v) {
         return typeof v === "function" ? v : function () { return v; };
     };
     // Based on:
     // http://www.johndcook.com/blog/2010/06/02/whats-so-hard-about-finding-a-hypotenuse/
-    science.hypot = function (x, y) {
+    export function hypot(x, y) {
         x = Math.abs(x);
         y = Math.abs(y);
         var max,
@@ -21,44 +22,15 @@
         else { max = y; min = x; }
         var r = min / max;
         return max * Math.sqrt(1 + r * r);
-    };
-    science.quadratic = function () {
-        var complex = false;
+    };  
 
-        function quadratic(a, b, c) {
-            var d = b * b - 4 * a * c;
-            if (d > 0) {
-                d = Math.sqrt(d) / (2 * a);
-                return complex
-                    ? [{ r: -b - d, i: 0 }, { r: -b + d, i: 0 }]
-                    : [-b - d, -b + d];
-            } else if (d === 0) {
-                d = -b / (2 * a);
-                return complex ? [{ r: d, i: 0 }] : [d];
-            } else {
-                if (complex) {
-                    d = Math.sqrt(-d) / (2 * a);
-                    return [
-                        { r: -b, i: -d },
-                        { r: -b, i: d }
-                    ];
-                }
-                return [];
-            }
-        }
+    /**
+     * Constructs a multi-dimensional array filled with zeroes.
+    */
+    export function zeroes(n: number) {
+        var i = -1;
+        var a: number[] = [];
 
-        quadratic.complex = function (x) {
-            if (!arguments.length) return complex;
-            complex = x;
-            return quadratic;
-        };
-
-        return quadratic;
-    };
-    // Constructs a multi-dimensional array filled with zeroes.
-    science.zeroes = function (n) {
-        var i = -1,
-            a = [];
         if (arguments.length === 1)
             while (++i < n)
                 a[i] = 0;
--- science.ts/app.ts
+++ science.ts/app.ts
@@ -7,6 +7,53 @@
 
     /**
      * Euler's constant.
-    */ 
+    */
     export const EULER = .5772156649015329;
+
+    function quadraticInternal(): IQuadratic {
+        var complex: boolean = false;
+        var quadratic: any = function (a: number, b: number, c: number): number[] {
+            var d = b * b - 4 * a * c;
+
+            if (d > 0) {
+                d = Math.sqrt(d) / (2 * a);
+                return complex
+                    ? [{ r: -b - d, i: 0 }, { r: -b + d, i: 0 }]
+                    : [-b - d, -b + d];
+            } else if (d === 0) {
+                d = -b / (2 * a);
+                return complex ? [{ r: d, i: 0 }] : [d];
+            } else {
+                if (complex) {
+                    d = Math.sqrt(-d) / (2 * a);
+                    return [
+                        { r: -b, i: -d },
+                        { r: -b, i: d }
+                    ];
+                }
+                return [];
+            }
+        }
+
+        quadratic.complex = function (x) {
+            if (!arguments.length) return complex;
+            complex = x;
+            return quadratic;
+        };
+
+        return quadratic;
+    };
+
+    export interface IQuadratic {
+        /**
+         * Do calculation
+        */
+        (a: number, b: number, c: number): number[];
+        /**
+         * Config function output
+        */
+        complex(config?: boolean): boolean;
+    }
+
+    export const quadratic: IQuadratic = quadraticInternal();
 }
