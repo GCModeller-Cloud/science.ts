f50f58c44b78a9bba49d48656837b91141cf1f42
xieguigang <xie.guigang@gcmodeller.org>
2019-10-11 17:04:45 +0800

    hclusterFunction

--- science.ts/modules/stats/stats.ts
+++ science.ts/modules/stats/stats.ts
@@ -97,4 +97,15 @@
         }
         return s / (n - 1);
     };
+
+    export function calculateCentroid(c1Size, c1Centroid, c2Size, c2Centroid) {
+        var newCentroid = [],
+            newSize = c1Size + c2Size,
+            n = c1Centroid.length,
+            i = -1;
+        while (++i < n) {
+            newCentroid[i] = (c1Size * c1Centroid[i] + c2Size * c2Centroid[i]) / newSize;
+        }
+        return newCentroid;
+    }
 }
