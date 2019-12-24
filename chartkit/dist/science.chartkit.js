70a04ee4d1c583c4173b0e152f39cce9f2c6a079
xieguigang <xie.guigang@gcmodeller.org>
2019-12-24 16:27:46 +0800

    compile script

--- chartkit/dist/science.chartkit.js
+++ chartkit/dist/science.chartkit.js
@@ -1,7 +1,17 @@
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -338,7 +348,7 @@ var D3;
                     }
                 }
                 else {
-                    chart.range = chart.settings.range.slice();
+                    chart.range = __spreadArrays(chart.settings.range);
                 }
                 chart.colorFunct = chart.getColorFunct(chart.settings.colors);
                 // Build Scale functions
